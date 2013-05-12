c9a58d6e632ddd41c5239e6e6b42012832b09491
Ian O'Hara <ianohara@gmail.com>
2013-05-12 16:25:06 +0000

    Got rid of (void*) cast.

--- typecasting.c
+++ typecasting.c
@@ -1,3 +1,18 @@
+/* Copyright 2013, Ian O'Hara
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 /* limits.h defines a lot of #define constants that give
  * the numerical limits (max, min) of the types on your
  * system.
@@ -25,7 +40,7 @@
 /* Print out the binary representation (1,0) of a number of
  * bytes at the address pointed to by *pval
  *
- * This prints from left to right with MSB to LSB, respectively
+ * This prints from left to right from MSB to LSB, respectively
  */
 void
 base2print(void *pval, size_t bytes)
@@ -78,33 +93,33 @@ main(int argc, char *argv[])
     printf("Section 1: Representing decimal number 1\n");
     hline(80);
     uint8_t ui8 = 1;
-    base2describe("uint8_t", (void*)&ui8, sizeof(ui8));
+    base2describe("uint8_t", &ui8, sizeof(ui8));
 
     uint16_t ui16 = 1;
-    base2describe("uint16_t", (void*)&ui16, sizeof(ui16));
+    base2describe("uint16_t", &ui16, sizeof(ui16));
 
     int8_t i8 = 1;
-    base2describe("int8_t", (void*)&i8, sizeof(i8));
+    base2describe("int8_t", &i8, sizeof(i8));
 
     nl();
     printf("Section 2: Representing the integer -1\n");
     hline(80);
     ui8 = (uint8_t)(-1);
-    base2describe("uint8_t", (void*)&ui8, sizeof(ui8));
+    base2describe("uint8_t", &ui8, sizeof(ui8));
     ui16 = (uint16_t)(-1);
-    base2describe("uint16_t", (void*)&ui16, sizeof(ui16));
+    base2describe("uint16_t", &ui16, sizeof(ui16));
     i8 = (int8_t)(-1);
-    base2describe("int8_t", (void*)&i8, sizeof(i8));
+    base2describe("int8_t", &i8, sizeof(i8));
 
 
     nl();
     printf("Section 3: Floating point 1.0.\n");
     hline(80);
     float fl1 = 1.0;
-    base2describe("float", (void*)&fl1, sizeof(fl1));
+    base2describe("float", &fl1, sizeof(fl1));
 
     double dbl1 = 1.0;
-    base2describe("double", (void*)&dbl1, sizeof(dbl1));
+    base2describe("double", &dbl1, sizeof(dbl1));
 
     printf("Now, lets look at a struct of uint8_t, int8_t, uint16_t, int16_t, uint32_t, int32_t members.");
     struct increasing_ints {
@@ -117,7 +132,7 @@ main(int argc, char *argv[])
     };
 
     struct increasing_ints increasing = {1, 1, 1, 1, 1, 1};
-    base2describe("struct increasing_ints", (void*)&increasing, sizeof(increasing));
+    base2describe("struct increasing_ints", &increasing, sizeof(increasing));
 
 
 
